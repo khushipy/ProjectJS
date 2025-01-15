ca9b2a167239cff1fc2d67331591b08834385245
Khushi Pal <114483555+khushipy@users.noreply.github.com>
2025-01-15 09:54:11 +0000

    update datatype

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -16,3 +16,52 @@ console.log(typeof "khushi")
 console.log(typeof null) // object 
 console.log(typeof undefined) // umdefined
 
+//primitive 
+// 7 types : String, Number, Boolean, null, Undefined, Symbols, BigInt
+const Id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(Id === anotherId ); // false
+const bigNum = 123456345n; // bigint
+console.log(typeof bigNum)
+let myObj = {
+    name: "Khushi",
+    age : 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction) // function
+
+/* Undefined = undefined
+   Null = Object
+   Boolean = Boolean 
+   Number = number 
+   String = string
+   object = object 
+   object = function
+*/
+
+let blah = ["blah","blahhh","blahhh"]
+console.log(typeof blah)
+console.log(blah)
+// ------------------------------------
+// stack (primitve)-> copy, Heap (non - primitive)-> reference (original) 
+// primitive - name "DOesn't changes the original as it gives the copy"
+let name1 = "pyhton"
+let name2 = name1;
+name2 = "Java"
+console.log(name1,name2)
+
+// non primitive - user "changes the detail in the original user also cause they refer to the same address and not a copy"
+let user1 = {
+    email: "user1@yeh",
+    age: 23,
+}
+let user2 = user1;
+user2.email = "user2@yeh";
+console.log(user1.email,user2.email)
+
+
