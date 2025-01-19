10b961a2b1b5e1bda7783ee29490943aef008e58
Khushi Pal <114483555+khushipy@users.noreply.github.com>
2025-01-19 21:47:06 +0530

    Update 01_array.js
    added part 2

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -17,4 +17,23 @@ console.log(n)
 console.log("B ",arr)
 const m = arr.splice(1,3)
 console.log(arr) // hehe splice removes the other elements
-console.log(m)
+console.log(m) 
+// part 2 ----
+const num = [1,2,3,4]
+const num2 = [2,3,6,5]
+let num3 = num.concat(num2)
+console.log(num3) // merges the two arrays
+num.push(num2)
+console.log(num) // push makes array in array
+let num4 = [...num,...num2,...num3] // to merge any no. of arrays
+console.log(num4)
+let another_array = [1,2,3,[20,45,67,[300,466,222,111]],33,412,45,56]
+const real_array = another_array.flat(Infinity)
+console.log(real_array)
+console.log(Array.isArray("Javascript"))
+console.log(Array.from("Javascript"));
+console.log(Array.from({name: "Js"})); //explain more
+let a = 23, b=33,c=78;
+console.log(Array.of(a,b,c));
+
+
